ac632323521330c6bf7086ca057512cb256fa361
geezgus <gutorap.gh@gmail.com>
2021-10-22 03:32:41 -0300

    new file:   assets/Read Me.txt 	new file:   assets/demo-files/demo.css 	new file:   assets/demo-files/demo.js 	new file:   assets/demo.html 	new file:   assets/fonts/beautysalon-icons.eot 	new file:   assets/fonts/beautysalon-icons.svg 	new file:   assets/fonts/beautysalon-icons.ttf 	new file:   assets/fonts/beautysalon-icons.woff 	new file:   assets/images/profile-picture.png 	new file:   assets/selection.json 	new file:   assets/style.css 	modified:   index.html 	modified:   main.js 	modified:   style.css

--- main.js
+++ main.js
@@ -1,26 +1,63 @@
-// Open and close menu when icon (hamburger || X) is clicked.
-const nav = document.querySelector('#header nav')
-const toggle = document.querySelectorAll('nav .toggle')
+/* Open and close menu when icon (hamburger || X) is clicked. */
+const nav = document.querySelector('#header nav');
+const toggle = document.querySelectorAll('nav .toggle');
 
 for (const element of toggle) {
-  element.addEventListener('click', () => nav.classList.toggle('show'))
+  element.addEventListener('click', () => {
+    nav.classList.toggle('show');
+  });
 }
 
-// Close menu when menu item is clicked.
-const links = document.querySelectorAll('nav ul li a')
+/* Close menu when menu item is clicked. */
+const links = document.querySelectorAll('nav ul li a');
 
 for (const link of links) {
-  link.addEventListener('click', () => nav.classList.remove('show'))
+  link.addEventListener('click', () => {
+    nav.classList.remove('show');
+  });
 }
 
-// Cast shadow from header when not on top of page.
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
+/* Cast shadow from header when not on top of page. */
+const header = document.querySelector('#header');
+const navHeight = header.offsetHeight;
 
 window.addEventListener('scroll', () => {
   if (window.scrollY >= 1) {
-    header.classList.add('scroll')
+    header.classList.add('scroll');
   } else {
-    header.classList.remove('scroll')
+    header.classList.remove('scroll');
   }
-})
+});
+
+/* Swiper: Make carousel slider for testimonials */
+const swiper = new Swiper('.swiper', {
+  // Slide style
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    dynamicBullets: true
+  },
+  loop: true,
+
+  // Control
+  mousewheel: true,
+  keyboard: true
+});
+
+/* ScrollReveal: Show elements on scroll */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 750,
+  reset: true
+});
+
+scrollReveal.reveal(
+  ` #home .image, #home .text,
+    #about .image, #about .text,
+    #services header, #services .card,
+    #testimonials header, #testimonials .testimonials,
+    #contact .text, #contact .links `,
+  { interval: 100 }
+);
